ca45471921e1e527bdb0558e80a3a3c1426e1d38
nedoletoff <nedoletoff@yandex.ru>
2021-09-21 20:27:07 +0300

    release version

--- double_list.hpp
+++ double_list.hpp
@@ -1,9 +1,256 @@
+#include <iostream>
+#include <string>
+#pragma once
+
 template <typename T>
-struct Node
+class Node
 {
-	T* data;
-	Node* prev;
-	Node* next;
+	private:
+		T data;
+		Node<T>* next;
+		Node<T>* prev;
+	public:
+		Node<T>()
+		{
+			next = NULL;
+			prev = NULL;
+		}
+
+		void set_prev(Node<T>& a)
+		{
+			prev = &a;
+		}
+
+		void set_next(Node<T>& a)
+		{
+			next = &a;
+		}
+
+		void set_data(const T a)
+		{
+			data = a;
+		}
+
+		T get_data()
+		{
+			return data;
+		}
+
+		Node<T>* get_next()
+		{
+			return next;
+		}
+
+		Node<T>* get_prev()
+		{
+			return prev;
+		}
+
+		void make_head()
+		{
+			prev = NULL;
+		}
+
+		void make_tail()
+		{
+			next = NULL;
+		}
+		void print()
+		{
+			std::cout << data;
+		}
+		T erase()
+		{
+			if (next != NULL)
+				next->set_prev(*get_prev());
+			if (prev != NULL)
+				prev->set_next(*get_next());
+			return data;
+		}
 };
 
+template <typename T>
+class DoubleList
+{
+	private:
+		Node<T>* head;
+		Node<T>* tail;
+		size_t size;
+	public:
+		DoubleList() 
+		{
+			size = 0;
+			head = NULL;
+			tail = NULL;
+		}
+
+		void push_back(const T d)
+		{
+			Node<T>* t = tail;
+			Node<T>* c = new Node<T>;
+			if (head == NULL)
+				head = c;
+
+			c->set_data(d);
+
+			if (tail != NULL)
+			{
+				t->set_next(*c);
+				c->set_prev(*t);
+			}
+			tail = c;
+			++size;
+//			std::cout << "push_back(" << d << ")" << std::endl;
+		}
+
+		T pop_back()
+		{
+			if (size)
+			{
+				Node<T>* cur = tail;
+				T d = cur->get_data();
+				if (tail->get_prev())
+				{
+					tail = tail->get_prev();
+					tail->make_tail();
+				}
+				else 
+				{
+					tail = NULL;
+					head = NULL;
+				}
+			
+				delete cur;
+				--size;
+	
+				return d;
+			}
+			else
+			{
+				throw 0.1;
+			}
+		}
+
+		DoubleList(DoubleList& a)
+		{
+			if (a.size == 0)
+			{
+				size = 0;
+				head = NULL;
+				tail = NULL;
+			}
+			else 
+			{
+				size = 0;
+				Node<T>* cur = a.head;
+				for (size_t i = 0; i <= size; ++i)
+				{
+					push_back(cur->get_data());
+					cur = cur->get_next();
+				}
+			}
+		}
+
+		DoubleList& operator=(DoubleList& a)
+		{
+			if (&a == this)
+				return *this;
+			if (a.size == 0)
+			{
+				size = 0;
+				head = NULL;
+				tail = NULL;
+			}
+			else 
+			{
+				size = 0;
+				Node<T>* cur = a.head;
+				for (size_t i = 0; i <= size; ++i)
+				{
+					push_back(cur->get_data());
+					cur = cur->get_next();
+				}
+			}
+			return *this;
+		}
+
+		~DoubleList()
+		{
+			try
+			{
+				while (tail) 
+				{
+					pop_back();
+				}
+			}
+			catch(double e)
+			{
+				std::cout << e << " list is empty" << std::endl;
+			}
+		}
+
+		void push_front(const T d)
+		{
+			Node<T>* h = head;
+			Node<T>* c = new Node<T>;
+			if (tail == NULL)
+				tail = c;
+			c->set_data(d);
+			if (head != NULL)
+			{
+				h->set_prev(*c);
+				c->set_next(*h);
+			}
+			head = c;
+			++size;
+//			std::cout << "push_front(" << d << ")" << std::endl;
+		}
+
+		void find_and_erase(const T value)
+		{
+			Node<T>* cur = head;
+			while (cur != NULL)
+			{
+				if (cur->get_data() == value)
+				{
+					if (head == cur)
+						head = cur->get_next();
+					if (tail == cur)
+						tail = cur->get_prev();
+					cur->erase();
+					--size;
+				}
+				cur = cur->get_next();
+			}
+		}
+
+		void at()
+		{
+			throw 1;
+		}
+
+		size_t get_size()
+		{
+			return size;
+		}
+
+		void print()
+		{
+			std::cout << "print()" << std::endl;
+			Node<T>* cur = head;
+			for (size_t i = 0; i < size; ++i)
+			{
+				cur->print();
+				std::cout << " \n";
+				cur = cur->get_next();
+			}
+		}
+};
+
+struct Point
+{
+	int x;
+	int y;
+	int z;
+};
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,55 @@
 #include "double_list.hpp"
-#include <iostream>
 
 int main()
 {
 
+	DoubleList<double> lst_int;
+
+	lst_int.push_back(2);	
+	lst_int.push_back(1);
+
+	
+	lst_int.push_front(3);
+	lst_int.push_front(1);
+	lst_int.print();
+	lst_int.push_back(1);
+	lst_int.push_back(2);
+	lst_int.push_back(1);
+	std::cout << "size - " << lst_int.get_size() << std::endl;
+	try 
+	{
+		lst_int.at();
+	}
+	catch(int i)
+	{
+		std::cout << i << std::endl;
+		std::cout << "Caught an exception" << std::endl;
+	}
+
+	lst_int.print();
+
+	std::cout << "find_and_erase(1)" << std::endl;
+	lst_int.find_and_erase(1);
+	lst_int.print();
+
+	DoubleList<Point> point_lst;
+	struct Point e;
+	e.x = 1; e.y = 2; e.z = 3;
+
+	point_lst.push_back(e);
+	e.x = 11;
+	std::cout << e.x << " " << e.y << " " << e.z << std::endl;
+	e = point_lst.pop_back();
+	std::cout << e.x << " " << e.y << " " << e.z << std::endl;
+
+	try 
+	{
+		point_lst.pop_back();
+	}
+	catch(double r)
+	{
+		std::cout << r << std::endl;
+		std::cout << "list is empty" << std::endl;
+	}
 	return 0;
 }
-
